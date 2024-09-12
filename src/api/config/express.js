90605ed0354a2ba1cac57f8cd3b0fc468c621548
h2green <brianolesnievicz@gmail.com>
2024-09-12 19:47:29 -0300

    feat(express): configurar Express
    - Configura Express en el archivo express.js
    - Relacionado con KAN-8 / KAN-35

---
+++ src/api/config/express.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import compress from 'compression';
+import methodOverride from 'method-override';
+import cors from 'cors';
+import helmet from 'helmet';
+import passport from 'passport';
+import routes from '../routes/index.route.js';
+import {logs} from './vars.js';
+import error from '../middlewares/error.js';
+
+const app = express();
+
+// request logging. dev: console | production: file
+app.use(morgan(logs));
+
+// parse body params and attach them to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// gzip compression
+app.use(compress());
+
+// lets you use HTTP verbs such as PUT or DELETE
+// in places where the client doesn't support it.
+app.use(methodOverride());
+
+// secure apps by setting various HTTP headers
+app.use(helmet());
+
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors());
+
+// enable authentication
+app.use(passport.initialize());
+passport.use('jwt', 'jwt');
+
+// mount all routes on /api path
+app.use('/api', routes);
+
+// if error is not an instanceOf APIError, convert it.
+app.use(error.converter);
+
+// catch 404 and forward to error handler
+app.use(error.notFound);
+
+// error handler, send stacktrace only during development
+app.use(error.handler);
+
+export default app;
