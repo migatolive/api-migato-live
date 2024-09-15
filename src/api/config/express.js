2fcc5595ce5e670dae92c0fd97037c80bd2f389e
h2green <brianolesnievicz@gmail.com>
2024-09-14 21:43:39 -0300

    refactor(express): update body parsing and improve error handling structure

--- src/api/config/express.js
+++ src/api/config/express.js
@@ -1,14 +1,15 @@
+import '../middlewares/error.js';
 import express from 'express';
 import morgan from 'morgan';
-import bodyParser from 'body-parser';
 import compress from 'compression';
 import methodOverride from 'method-override';
 import cors from 'cors';
 import helmet from 'helmet';
 import passport from 'passport';
 import routes from '../routes/index.route.js';
-import {logs} from './vars.js';
+import { logs } from './vars.js';
 import error from '../middlewares/error.js';
+import { jwt } from './passport.js';
 
 const app = express();
 
@@ -16,8 +17,8 @@ const app = express();
 app.use(morgan(logs));
 
 // parse body params and attach them to req.body
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // gzip compression
 app.use(compress());
@@ -34,18 +35,19 @@ app.use(cors());
 
 // enable authentication
 app.use(passport.initialize());
-passport.use('jwt', 'jwt');
+passport.use('jwt', jwt);
 
 // mount all routes on /api path
 app.use('/api', routes);
 
-// if error is not an instanceOf APIError, convert it.
-app.use(error.converter);
 
-// catch 404 and forward to error handler
-app.use(error.notFound);
-
-// error handler, send stacktrace only during development
-app.use(error.handler);
+// error logger
+app.use(error.errorLogger);
+// error converter
+app.use(error.errorConverter);
+// error handler
+app.use(error.errorHandler);
+// error 404
+app.use(error.errorNotFound);
 
 export default app;
