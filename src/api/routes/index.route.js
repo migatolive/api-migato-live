451af5b6932e5da59420d64f0dfb617a6f094713
h2green <brianolesnievicz@gmail.com>
2024-09-18 22:54:34 -0300

    refactor(routes): add userRoutes

--- src/api/routes/index.route.js
+++ src/api/routes/index.route.js
@@ -1,12 +1,13 @@
 import {Router} from 'express';
 // import contactRoutes from '../components/contact/routes/contact.routes.js';
 import authRoutes from '../components/auth/routes/auth.routes.js';
-// import userRoutes from '../components/user/routes/user.routes.js';
+import userRoutes from '../components/user/routes/user.routes.js';
 import bookRoutes from '../components/book/routes/book.routes.js';
 
 const router = Router();
 
 router.use('/v1/auth', authRoutes);
 router.use('/v1/book', bookRoutes);
+router.use('/v1/user', userRoutes);
 
 export default router;
