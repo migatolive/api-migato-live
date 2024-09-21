e0ba3c08adf776f85a7a49f8c779f76fd3859773
h2green <brianolesnievicz@gmail.com>
2024-09-21 20:46:28 -0300

    fix(middleware): hide error stack trace in production environment

--- src/api/middlewares/error.js
+++ src/api/middlewares/error.js
@@ -1,4 +1,4 @@
-import { ValidationError } from 'sequelize';
+import {ValidationError} from 'sequelize';
 import httpStatus from "http-status";
 import APIError from '../utils/api-error.js';
 
@@ -59,8 +59,11 @@ export function errorHandler(err, req, res, next) {
         code: err.status,
         message: err.message || httpStatus[err.status],
         errors: err.errors,
-        stack: err.stack,
     };
+
+    if (process.env.NODE_ENV === 'development') {
+        response.stack = err.stack;
+    }
 
     res.status(err.status);
     res.json(response);
